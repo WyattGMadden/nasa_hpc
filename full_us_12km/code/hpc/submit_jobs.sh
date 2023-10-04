f1e8421331d01bee8cdb65d6dd8da7f81a21901e
Wyatt Madden <wyattgmadden@gmail.com>
2023-10-04 17:44:09 -0400

    updated file path

--- full_us_12km/code/hpc/submit_jobs.sh
+++ full_us_12km/code/hpc/submit_jobs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-tail -n +2 ../data/job_params_full_grid.csv | while IFS=',' read -r param1 param2
+tail -n +2 ../../output/data/job_params.csv | while IFS=',' read -r param1 param2
 do
     sbatch --export=PARAM1="$param1",PARAM2="$param2" run_single_job.sh
 done
