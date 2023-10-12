a181c60aac8d726076f8b5777a14237ec46b5205
wmadden <wmadden@clogin01.cl.emory.edu>
2023-10-12 12:02:14 -0400

    updated sbatch param

--- ca_1km_w_cov/code/hpc/run_single_job.sh
+++ ca_1km_w_cov/code/hpc/run_single_job.sh
@@ -5,7 +5,7 @@
 #SBATCH --ntasks=1                 # total number of tasks across all nodes
 #SBATCH --cpus-per-task=1          # cpu-cores per task (>1 if multi-threaded tasks)
 #SBATCH --mem=8G                   # total memory per node (4 GB per cpu-core is default)
-#SBATCH -o ../../output/logs/grm-full-us-%A_%a.out  # output file format (%A is jobID, %a is task index)
+#SBATCH -o ../../output/logs/grm-ca-%A_%a.out  # output file format (%A is jobID, %a is task index)
 
 module purge
 module load R
