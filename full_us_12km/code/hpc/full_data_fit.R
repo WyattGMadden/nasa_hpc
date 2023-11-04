fe0d9788982e74554e17e604f5db10c33af20724
Wyatt Madden <wyattgmadden@gmail.com>
2023-11-04 11:12:50 -0400

    updated full us hpc params, init and priors for spatial params

--- full_us_12km/code/hpc/full_data_fit.R
+++ full_us_12km/code/hpc/full_data_fit.R
@@ -8,8 +8,16 @@ full_us_run <- function(matern.nu, cv) {
     thin <- 4
 
 
-    discrete.theta.alpha.values <- seq(10, 60, 2.5)       
-    discrete.theta.beta.values <- seq(10, 60, 2.5)       
+    discrete.theta.alpha.values <- seq(0.1, 1, 0.025)       
+    discrete.theta.beta.values <- seq(0.1, 1, 0.025)       
+    theta_ab <- 0.05
+    theta_init <- 0.5
+    if (matern.nu == 0.5) {
+        discrete.theta.alpha.values <- discrete.theta.alpha.values * 10
+        discrete.theta.beta.values <- discrete.theta.beta.values * 10
+        theta_ab <- 0.005
+        theta_init <- 0.5
+    }
 
     time_fit <- system.time({
     ctm_fit <- grmbayes::grm(Y = obs_full_us$pm_aqs,
@@ -23,6 +31,16 @@ full_us_run <- function(matern.nu, cv) {
                              discrete.theta.gibbs = F,
                              discrete.theta.alpha.values = discrete.theta.alpha.values,
                              discrete.theta.beta.values = discrete.theta.beta.values,
+                             theta.alpha.a = theta_ab,
+                             theta.alpha.b = theta_ab,
+                             theta.beta.a = theta_ab,
+                             theta.beta.b = theta_ab,
+                             tau.alpha.a = 0.005,
+                             tau.alpha.b = 0.005,
+                             tau.beta.a = 0.005,
+                             tau.beta.b = 0.005,
+                             tau.alpha.tune = 0.05,
+                             tau.beta.tune = 0.05,
                              coords = obs_full_us[, c("x", "y")],
                              space.id = obs_full_us$space_id,
                              time.id = obs_full_us$time_id,
@@ -42,6 +60,16 @@ full_us_run <- function(matern.nu, cv) {
                              discrete.theta.gibbs = F,
                              discrete.theta.alpha.values = discrete.theta.alpha.values,
                              discrete.theta.beta.values = discrete.theta.beta.values,
+                             theta.alpha.a = theta_ab,
+                             theta.alpha.b = theta_ab,
+                             theta.beta.a = theta_ab,
+                             theta.beta.b = theta_ab,
+                             tau.alpha.a = 0.005,
+                             tau.alpha.b = 0.005,
+                             tau.beta.a = 0.005,
+                             tau.beta.b = 0.005,
+                             tau.alpha.tune = 0.05,
+                             tau.beta.tune = 0.05,
                              coords = obs_full_us[, c("x", "y")],
                              space.id = obs_full_us$space_id,
                              time.id = obs_full_us$time_id,
