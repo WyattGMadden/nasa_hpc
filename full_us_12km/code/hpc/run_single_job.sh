6e8404592bbdc641f3b5886f5bf98ac18f1659a8
wmadden <wmadden@clogin01.cl.emory.edu>
2023-12-05 10:18:16 -0500

    changes

--- full_us_12km/code/hpc/run_single_job.sh
+++ full_us_12km/code/hpc/run_single_job.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 #SBATCH --job-name=_oOUSOo_     # create a short name for your job
 #SBATCH --nodes=1                  # node count
-#SBATCH --partition=week-long-cpu
+#SBATCH --partition=chang
 #SBATCH --ntasks=1                 # total number of tasks across all nodes
 #SBATCH --cpus-per-task=1          # cpu-cores per task (>1 if multi-threaded tasks)
 #SBATCH --mem=48G                   # total memory per node (4 GB per cpu-core is default)
 #SBATCH -o ../../output/logs/grm-full-us-%A_%a.out  # output file format (%A is jobID, %a is task index)
-
 module purge
 module load R
 
-Rscript -e "source('full_data_fit.R'); full_us_run(matern.nu=as.numeric(Sys.getenv('PARAM1')), cv=Sys.getenv('PARAM2'))"
+Rscript -e "print(.libPaths()); source('full_data_fit.R'); full_us_run(matern.nu=as.numeric(Sys.getenv('PARAM1')), cv=Sys.getenv('PARAM2'))"
 
