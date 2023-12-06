ef138d443a1f13bf5308e48be342e548cccb64d9
wmadden <wmadden@clogin01.cl.emory.edu>
2023-12-06 15:53:52 -0500

    ca pred job

--- ca_1km_w_cov/code/hpc/preds.R
+++ ca_1km_w_cov/code/hpc/preds.R
@@ -3,23 +3,23 @@ full_preds <- function() {
     #best rmse matern.nu
     fit <- readRDS("../../output/results/fits/fit_0.5_ordinary.RDS")$ctm_fit
 
+    obs <- readRDS("../../data/created/obs.rds")
     pred_dat <- readRDS("../../data/created/preds.rds")
 
 
 
     preds <- grmbayes::grm_pred(grm.fit = fit,
                                 X.pred = pred_dat$aod.final,
-                                L.pred = pred_dat[, c("elevation", "population")],
-                                M.pred = pred_dat[, c("cloud", "v_wind", "hpbl", 
-                                                 "u_wind", "short_rf", 
-                                                 "humidity_2m")],
+                                L = obs[, c("elevation", "population")],
+                                M = obs[, c("cloud", "v_wind", "hpbl",
+                                            "u_wind", "short_rf", "humidity_2m")],
                                 coords.Y = obs[, c("x", "y")],
                                 space.id.Y = obs$space_id,
                                 coords.pred = pred_dat[, c("x", "y")],
                                 space.id = pred_dat$space_id,
                                 time.id = pred_dat$time_id,
                                 spacetime.id = pred_dat$spacetime_id,
-                                n.iter = 10,
+                                n.iter = 2000,
                                 verbose = T,
                                 in.sample = TRUE)
 
