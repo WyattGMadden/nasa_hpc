651fe607f4b633665f9decbe8c55d616c39762da
Wyatt Madden <wyattgmadden@gmail.com>
2024-03-01 09:43:29 -0500

    buffer size code

--- ca_1km_w_cov/code/pre_process/prelim_fit_explore.R
+++ ca_1km_w_cov/code/pre_process/prelim_fit_explore.R
@@ -1,33 +1,49 @@
 
 
 
-dirs <- list.files("../../data/created/prelim_fit/",
+dirs <- list.files("../../data/created/prelim_fit",
                    full.names = T)
-fit_mat <- readRDS("../../data/created/prelim_fits.rds")
 
 all <- lapply(dirs,
-              readRDS)
-
+              function(x) {
+                  temp <- readRDS(x)
+                  temp$matern_nu <- substr(x, 
+                                           nchar(x) - 6, 
+                                           nchar(x) - 4) |>
+                    as.numeric()
+                  return(temp)
+              })
 
 
 others_out <- lapply(all,
                  function(x) {
                      fit <- x$others
-                     fit$matern_nu <- x$matern.nu
+                     fit$matern_nu <- x$matern_nu
                      fit$iter <- 1:nrow(fit)
                      return(fit)
                     }
                  ) |>
     (\(.) Reduce(rbind, .))()
 
+temp <- all[[2]]
+temp$theta.acc
+temp$theta.acc
 
+dinvgamma <- function(x, alpha, beta) dgamma(1/x, alpha, beta) * (1/x)^2
+dgamma(1.5, 0.5, 0.5)
 
-plot(x, y, type = "l")
+x <- seq(0.01, 200, 0.01)
+y <- dgamma(x, 0.005, 0.005)
+y <- dinvgamma(x, 3, 200)
+plot(x, y)
+
 library(ggplot2)
+library(tidyverse)
 others_out |>
     ggplot(aes(x = iter, y = theta.alpha)) + 
     geom_line() +
     facet_grid(matern_nu ~ ., scales = "free")
+
 others_out |>
     ggplot(aes(x = iter, y = theta.beta)) + 
     geom_line() +
@@ -60,18 +76,21 @@ others_out |>
 #based on max(theta_alpha, theta_beta), nu = 0.5
 #correlation of 0.7 and 0.3
 
-mean_theta_alpha <- mean(others_out$theta.alpha[others_out$matern_nu == 0.5])
-mean_theta_beta <- mean(others_out$theta.beta[others_out$matern_nu == 0.5])
-mean_theta <- max(c(mean_theta_alpha, mean_theta_beta))
+get_buffer_cor <- function(nu, corr, others_out) {
+    others_out_tb <- others_out[others_out$iter > 200 & (others_out$iter %% 4 == 0),]
 
+    mean_theta_alpha <- mean(others_out_tb$theta.alpha[others_out_tb$matern_nu == nu])
+    mean_theta_beta <- mean(others_out_tb$theta.beta[others_out_tb$matern_nu == nu])
+    mean_theta <- max(c(mean_theta_alpha, mean_theta_beta))
 
+    #inverse correlation
+    buffer <- - mean_theta * log(corr)
+    return(buffer)
+}
+buffer_params <- expand.grid(nu = seq(0.5, 2.5, 1), 
+                           corr = c(0.7, 0.3)) |>
+    group_by(nu, corr) |>
+    mutate(distance = get_buffer_cor(nu, corr, others_out))
 
-#inverse correlation
-distance <- - mean_theta * log(correlation)
-buffer_0.8 <- - mean_theta * log(0.7)
-buffer_0.5 <- - mean_theta * log(0.3)
+write.csv(buffer_params, "../../data/created/buffer_params.csv")
 
-
-
-#summary(others_out$theta.alpha)
-#summary(others_out$theta.beta)
